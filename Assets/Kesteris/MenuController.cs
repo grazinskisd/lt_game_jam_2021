a8892026b2aad4e4cbd2f4957aeafdad78778967
Kestv <kestutis.ramulionis@gmail.com>
2021-01-30 19:44:15 +0200

    Menu and font

---
+++ Assets/Kesteris/MenuController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
