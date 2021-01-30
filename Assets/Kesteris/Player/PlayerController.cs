f994d710f657a61d54793c6af3bd1c7378fb37ba
Kestv <kestutis.ramulionis@gmail.com>
2021-01-30 17:59:48 +0200

    Player controls and stun

--- Assets/Kesteris/Player/PlayerController.cs
+++ Assets/Kesteris/Player/PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     float MoveSpeed;
+    [SerializeField]
+    float StunDuration;
+    float LastStunTime;
     Text TextW;
     Text TextA;
     Text TextS;
@@ -17,9 +20,11 @@ public class PlayerController : MonoBehaviour
 
     bool CanHandOverPost;
     bool CanPickUpPost;
+    bool IsAbleToMove = true;
     int TriggeredHouseNo = 0;
     PostController PostCtrl;
     KeyCode NewKey = 0;
+    int LastModifiedKeyIndex = 0;
 
     KeyCode[] Keys = {
         KeyCode.Q, KeyCode.W, KeyCode.E, KeyCode.R, KeyCode.T, KeyCode.Y, KeyCode.U, KeyCode.I, KeyCode.O, KeyCode.P,
@@ -45,37 +50,48 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        var vertical = Input.GetKey(TakenKeys[0]) ? 1 : Input.GetKey(TakenKeys[2]) ? -1 : 0;
-        var horizontal = Input.GetKey(TakenKeys[3]) ? 1 : Input.GetKey(TakenKeys[1]) ? -1 : 0;
-        float rotation = 0;
-        if (horizontal > 0 && vertical > 0)
-            rotation = 45;
-        else if (horizontal > 0 && vertical < 0)
-            rotation = 135;
-        else if (horizontal < 0 && vertical > 0)
-            rotation = -45;
-        else if (horizontal < 0 && vertical < 0)
-            rotation = -135;
-        else if (vertical > 0)
-            rotation = 0;
-        else if (vertical < 0)
-            rotation = 180;
-        else if (horizontal > 0)
-            rotation = 90;
-        else if (horizontal < 0)
-            rotation = -90;
-
-        if (vertical != 0 || horizontal != 0)
+        if (IsAbleToMove)
         {
-            Animator.SetBool("IsRunning", true);
-            transform.eulerAngles = new Vector3(0, rotation, 0);
-            Vector3 movement = transform.forward * MoveSpeed / 10;
-            Controller.Move(movement);
+            var vertical = Input.GetKey(TakenKeys[0]) ? 1 : Input.GetKey(TakenKeys[2]) ? -1 : 0;
+            var horizontal = Input.GetKey(TakenKeys[3]) ? 1 : Input.GetKey(TakenKeys[1]) ? -1 : 0;
+            float rotation = 0;
+            if (horizontal > 0 && vertical > 0)
+                rotation = 45;
+            else if (horizontal > 0 && vertical < 0)
+                rotation = 135;
+            else if (horizontal < 0 && vertical > 0)
+                rotation = -45;
+            else if (horizontal < 0 && vertical < 0)
+                rotation = -135;
+            else if (vertical > 0)
+                rotation = 0;
+            else if (vertical < 0)
+                rotation = 180;
+            else if (horizontal > 0)
+                rotation = 90;
+            else if (horizontal < 0)
+                rotation = -90;
+
+            if (vertical != 0 || horizontal != 0)
+            {
+                Animator.SetBool("IsRunning", true);
+                transform.eulerAngles = new Vector3(0, rotation, 0);
+                Vector3 movement = transform.forward * MoveSpeed / 10;
+                Controller.Move(movement);
+            }
+            else
+            {
+                Animator.SetBool("IsRunning", false);
+            }
         }
         else
         {
-            Animator.SetBool("IsRunning", false);
+            if (Time.time - LastStunTime > StunDuration)
+            {
+                IsAbleToMove = true;
+            }
         }
+
 
         if (Input.GetKeyDown(KeyCode.E) && CanHandOverPost)
         {
@@ -108,6 +124,12 @@ public class PlayerController : MonoBehaviour
         LooseControl();
     }
 
+    public void Stun()
+    {
+        IsAbleToMove = false;
+        LastStunTime = Time.time;
+    }
+
     private void HandOverPost()
     {
         PostCtrl.HandOverPost(TriggeredHouseNo);
@@ -122,6 +144,11 @@ public class PlayerController : MonoBehaviour
     {
         var newKeyIndex = Random.Range(0, Keys.Length - 1);
         var replacingKeyIndex = Random.Range(0, TakenKeys.Count - 1);
+        if (replacingKeyIndex == LastModifiedKeyIndex)
+        {
+            LooseControl();
+            return;
+        }
         
         NewKey = Keys[newKeyIndex];
         var replacingKey = TakenKeys[replacingKeyIndex];
