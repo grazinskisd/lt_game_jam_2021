64924f6d0d7c3c41429d18e438cca5b6fc40b3c9
Dalius Grazinskis <grazinskisd@gmail.com>
2021-01-30 19:34:05 +0200

    Add some sound effects

--- Assets/Cricket/Scripts/AttackBehaviour.cs
+++ Assets/Cricket/Scripts/AttackBehaviour.cs
@@ -3,15 +3,24 @@ using UnityEngine;
 
 public abstract class AttackBehaviour : MonoBehaviour
 {
+    public AudioClip triggerEffect;
     public PlayerTrigger trigger;
+
+    private AudioManager _audioManager;
 
     protected virtual void Awake()
     {
         trigger.onPlayerEntered.AddListener(OnPlayerEntered);
+        _audioManager = FindObjectOfType<AudioManager>();
     }
 
     protected virtual void OnPlayerEntered(PlayerController player)
     {
+        var audioSource = player.GetComponent<AudioSource>();
+        if(audioSource != null && triggerEffect != null)
+        {
+            audioSource.PlayOneShot(triggerEffect, _audioManager.effectVolume);
+        }
         player.Stun();
     }
 
