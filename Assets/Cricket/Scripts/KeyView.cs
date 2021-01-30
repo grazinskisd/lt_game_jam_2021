22a2e2ff6d3088b31f899e7c509bd8b6c2cdb872
Dalius Grazinskis <grazinskisd@gmail.com>
2021-01-30 20:28:14 +0200

    Add key view for player input display

---
+++ Assets/Cricket/Scripts/KeyView.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class KeyView : MonoBehaviour
+{
+    [SerializeField]
+    private Text _text = null;
+
+    [SerializeField]
+    private RectTransform _scaleTarget = null;
+
+    public Color Color
+    {
+        set
+        {
+            _text.color = value;
+            PlayScaleAnimation();
+        }
+    }
+
+    public string Text
+    {
+        set
+        {
+            _text.text = value;
+            PlayScaleAnimation();
+        }
+
+        get
+        {
+            return _text.text;
+        }
+    }
+
+    private void PlayScaleAnimation()
+    {
+        
+    }
+}
